eec4a7558d4d7606269a2732209c5e011ee293af
PATRON Mickael <mickael_patron@hotmail.com>
2025-01-16 15:13:50 +0100

    save

--- compile-all.sh
+++ compile-all.sh
@@ -4,4 +4,4 @@ git fetch --tags
 git random
 podman build --tag mpatron/kdownload:${VERSION} --file ./Dockerfile
 podman save localhost/mpatron/kdownload:${VERSION} -o /mnt/c/Temp/kdownload-${VERSION}.tar
-
+echo scp /mnt/c/Temp/kdownload-${VERSION}.tar mpatron@10.6.193.182:/home/mpatron/Documents
