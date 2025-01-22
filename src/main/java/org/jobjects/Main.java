d5fd0d4bf249a23b4514831eb9ab4d75d616c01a
PATRON Mickael <mickael_patron@hotmail.com>
2025-01-22 15:41:39 +0100

    save

--- src/main/java/org/jobjects/Main.java
+++ src/main/java/org/jobjects/Main.java
@@ -1,11 +1,7 @@
 package org.jobjects;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Main {
     public static void main(String[] args) {
-        String chaine = "HTTP/deborah.jobjects.org";
-        String[] chaines = StringUtils.split(chaine, "/");
         System.out.println("Hello World!");
       }
     
