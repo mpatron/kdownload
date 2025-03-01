59390d8ec6a2a77c85b4ba2db4d4af9ff876e7f4
PATRON Mickael <mickael_patron@hotmail.com>
2025-03-02 00:57:24 +0100

    save

--- podman/docker_entrypoint_start-quarkus.sh
+++ podman/docker_entrypoint_start-quarkus.sh
@@ -5,6 +5,7 @@
 KD_KEYTAB_FILE="${KEYTAB_FILE:-/home/mickael/tmp/http.deborah.jobjects.org.keytab}"
 KD_SERVICE_PRINCIPAL_NAME="${SERVICE_PRINCIPAL_NAME:-HTTP/deborah.jobjects.org}"
 KD_SERVICE_PRINCIPAL_REALM="${SERVICE_PRINCIPAL_REALM:-JOBJECTS.ORG}"
+KD_UPLOAD_DIR="${UPLOAD_DIR:-/tmp}"
 
 echo "**********************************"
 echo "-> Log: Root path: '$(pwd)'"
@@ -14,11 +15,13 @@ echo "-> Log: Check env variables:"
 echo "     - KD_KEYTAB_FILE:             '$KD_KEYTAB_FILE'"
 echo "     - KD_SERVICE_PRINCIPAL_NAME:  '$KD_SERVICE_PRINCIPAL_NAME'"
 echo "     - KD_SERVICE_PRINCIPAL_REALM: '$KD_SERVICE_PRINCIPAL_REALM'"
+echo "     - KD_UPLOAD_DIR:              '$KD_UPLOAD_DIR'"
 
 QUARKUS_OPS=
 QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.kerberos.keytab-path=$KD_KEYTAB_FILE"
 QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.kerberos.service-principal-name=$KD_SERVICE_PRINCIPAL_NAME"
 QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.kerberos.service-principal-realm=$KD_SERVICE_PRINCIPAL_REALM"
+QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.http.body.uploads-directory=$KD_UPLOAD_DIR"
 QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.kerberos.debug=true"
 QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.kerberos.enabled=true"
 QUARKUS_OPS="$QUARKUS_OPS -Dquarkus.http.host=0.0.0.0"
