630d180aed8579388839bc4ea103d91985e526b6
PATRON Mickael <mickael_patron@hotmail.com>
2025-03-07 10:44:15 +0100

    save

--- src/test/java/org/jobjects/health/ReadinessHealthCheckTest.java
+++ src/test/java/org/jobjects/health/ReadinessHealthCheckTest.java
@@ -1,17 +1,25 @@
 package org.jobjects.health;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 class ReadinessHealthCheckTest {
     @Test
     void testHelloEndpoint() {
-        given().when().get("/q/health/ready").then().statusCode(200).body(containsString("Ready health check"));
+        given()
+                .contentType(ContentType.JSON)
+                .when().get("/q/health/ready")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("UP"))
+                .body("checks[0].name", equalTo("Ready health check"))
+                .body("checks[0].data.service-principal-name", equalTo("HTTP/localhost"))
+                .body("checks[0].data.service-principal-realm", equalTo("EXAMPLE.COM"));
     }
-
 }
